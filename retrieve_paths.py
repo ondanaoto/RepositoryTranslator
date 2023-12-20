d864e57cd2811bae43d41e6228a5d128007c5438
Ondanaoto <n.onda.go20@gmail.com>
2023-12-20 16:47:01 +0900

    PathRetrieverクラスの実装

--- retrieve_paths.py
+++ retrieve_paths.py
@@ -1,17 +1,28 @@
 import os, argparse
 from typing import List
+from dataclasses import dataclass
 
 from extensions import FileExtension
 
-def extract_file_paths(directory, file_extension_list: List[FileExtension]) -> None:
-    file_path_list = [
-        os.path.join(root, file)
-        for root, _, files in os.walk(directory)
-        for file in files
-        if any(file.endswith(ext.value) for ext in file_extension_list)
-    ]
-    with open("file_paths.txt", "w") as file:
-        file.write("\n".join(file_path_list))
+@dataclass(frozen=True)
+class PathRetriever:
+    
+    target_file_exts: List[FileExtension]
+    
+    def retrieve(self, directory: str) -> None:
+        file_path_list = [
+            os.path.join(root, file)
+            for root, _, files in os.walk(directory)
+            for file in files
+            if any(file.endswith(ext.value) for ext in self.target_file_exts)
+        ]
+        with open("file_paths.txt", "w") as file:
+            file.write("\n".join(file_path_list))
+            
+    @classmethod
+    def from_file_ext_strs(cls, ext_strs: List[str]) -> "PathRetriever":
+        file_extensions = list(map(FileExtension.from_str, ext_strs))
+        return PathRetriever(file_extensions)
         
 def main():
     parser = argparse.ArgumentParser(description='Extract files from a directory')
@@ -19,9 +30,8 @@ def main():
     parser.add_argument('--extensions', nargs='+', type=str, default=['.md'], help='file extensions to extract')
     args = parser.parse_args()
     
-    file_extensions = list(map(FileExtension.from_str, args.extensions))
-    
-    extract_file_paths(args.directory, file_extensions)
+    retriever = PathRetriever.from_file_ext_strs(args.extensions)
+    retriever.retrieve(args.directory)
     
 if __name__ == "__main__":
     main()
